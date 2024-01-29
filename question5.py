caebf41452019ce9eb98ef2d35d31796b9ad9839
AnkithaSreeramoju <83494220+AnkithaSreeramoju@users.noreply.github.com>
2024-01-29 15:28:42 -0500

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,8 +1,9 @@
+
 def question5_1():
     """
     answer = {
-            'bin1': [d1,d2,d3...]
-            'bin2': [e1,...],
+            'bin1': [UserID1, UserID2, UserID3, UserID4, UserID5]
+            'bin2': [UserID6, UserID7, UserID8],
             'bin3': [f1,...]
     }
     where di, ei, fi are integers. 
