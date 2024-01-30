3aad03876137766e69b9c425ac839c77688f1f04
AnkithaSreeramoju <83494220+AnkithaSreeramoju@users.noreply.github.com>
2024-01-30 00:02:16 -0500

    Update question10.py

--- question10.py
+++ question10.py
@@ -1,25 +1,33 @@
 def question10_1():
     answer = False
+    # Noise can still be a problem with count data as it may introduce variability and affect the accuracy of the analysis.
     return answer
 
 def question10_2():
     answer = True
+    # The correlation coefficient is indeed a value between -1 and 1, indicating the strength and direction of a linear relationship between two sets of real values.
     return answer
 
 def question10_3():
     answer = False
+    # Aggregating a daily time series may be more appropriate for reducing size while preserving important patterns compared to simple sampling, which might discard valuable information.
+
     return answer
 
 def question10_4():
-    answer = True
+    answer = False
+    # Noise and outliers are not always the same; outliers are extreme values, while noise refers to random variations.
     return answer
 
 def question10_5():
     answer = False
+    #An object being an outlier doesn't necessarily mean it is noise; outliers can represent genuine, significant deviations from the norm.
+
     return answer
 
 def question10_6():
     answer = False
+    #An asymmetric binary attribute has distinct categories with different meanings, while a binary attribute with values 0 or 1 is symmetric in nature.
     return answer
 
 def question10_7():
@@ -28,12 +36,15 @@ def question10_7():
 
 def question10_8():
     answer = False
+    # Discrete variables can be ratio if they have a true zero point, such as counts of events or quantities.
     return answer
 
 def question10_9():
     answer = False
+    #Quantitative variables can be discrete or continuous; they are not synonymous.
     return answer
 
 def question10_10():
     answer = False
+    #Converting ordinal variables to asymmetric binary variables may result in information loss as the ordinal nature and order of categories may not be preserved.
     return answer
