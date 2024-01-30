4e2187ddd9720ed28b4e39f8ebd0809e14bca8a8
AnkithaSreeramoju <83494220+AnkithaSreeramoju@users.noreply.github.com>
2024-01-29 22:47:00 -0500

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,31 +1,32 @@
 def question3_1():
-    answer = ['Continuous', 'Quantitative', 'Ratio']
+    answer = ['Discrete', 'Quantitative', 'Ratio']
     return answer
 
 def question3_2():
-     answer = ['Discrete', 'Qualitative', 'Ordinal']
+    answer = ['Discrete', 'Qualitative', 'Ordinal']
     return answer
 
 def question3_3():
-     answer = ['Discrete', 'Qualitative', 'Nominal']
-    return answer 
+    answer = ['Discrete', 'Qualitative', 'Ordinal']
+    return answer
 
 def question3_4():
-     answer = ['Continuous', 'Quantitative', 'Ratio']
+    answer = ['Continuous', 'Quantitative', 'Ratio']
     return answer
 
 def question3_5():
-     answer = ['Discrete', 'Qualitative', 'Nominal']
+    answer = ['Discrete', 'Qualitative', 'Nominal']
     return answer
 
 def question3_6():
-     answer = ['Continuous', 'Quantitative', 'Ratio']
+    answer = ['Continuous', 'Quantitative', 'Ratio']
     return answer
 
 def question3_7():
-     answer = ['Continuous', 'Qualitative', 'Ordinal']
+    answer = ['Binary', 'Qualitative', 'Nominal']
     return answer
 
 def question3_8():
-     answer = ['Discrete', 'Qualitative', 'Ordinal']
+    answer = ['Discrete', 'Qualitative', 'Nominal']
     return answer
+
