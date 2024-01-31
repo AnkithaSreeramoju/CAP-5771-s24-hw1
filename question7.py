013cfc16f80c8c2509a802d2e37be994a59d817b
AnkithaSreeramoju <83494220+AnkithaSreeramoju@users.noreply.github.com>
2024-01-30 21:49:32 -0500

    Update question7.py

--- question7.py
+++ question7.py
@@ -7,8 +7,7 @@ def question7_2():
     return answer
 
 def question7_3():
-    answer = [(0., 4.), 
-              (4., 5.), 
-              (5., 8.), 
-              (8., 'infinity')]    
+    answer = [(0., 2.), (2., 3.), (3.,5.), (5., 'infinity')]
+    '''The representation of inifinity is 'infinity'. 
+    #bi, ei are floats or 'infinity'. '''   
     return answer
