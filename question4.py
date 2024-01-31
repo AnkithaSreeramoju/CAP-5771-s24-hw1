e157d415b08ae3146032639379b41c88c3f833f3
AnkithaSreeramoju <83494220+AnkithaSreeramoju@users.noreply.github.com>
2024-01-30 22:50:39 -0500

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,31 +1,31 @@
 def question4_1():
-    answer = ['Nominal', 'Oridinal']
+    answer = ['nominal', 'Nominal']
     return answer
 
 def question4_2():
-    answer = [ 'Ordinal','ratio']
+    answer = ['ordinal', 'ratio']
     return answer
 
 def question4_3():
-    answer = ['Ratio', 'Oridinal'] 
-    return answer
+    answer = ['ratio', 'ordinal']
+    return answer 
 
 def question4_4():
-    answer = ['Ratio', 'Oridinal']
+    answer = ['ratio', 'ordinal']
     return answer
 
 def question4_5():
-    answer = ['Ratio', 'Ratio']
+    answer = ['ratio', 'ratio']
     return answer
 
 def question4_6():
-    answer = ['Ratio', 'Oridinal']
+    answer = ['ratio', 'ordinal']
     return answer
 
 def question4_7():
-    answer = ['Ratio', 'Interval']
+    answer = ['ratio', 'interval']
     return answer
 
 def question4_8():
-    answer = ['Ratio', 'ratio']
+    answer = ['ratio', 'ratio']
     return answer
